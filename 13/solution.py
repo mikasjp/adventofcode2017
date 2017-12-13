dfa28ce79836eb942e3b0f9377ec5d180e4c15c9
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-12-13 15:39:35 +0100

    Day 13 - part 2 attempt

--- 13/solution.py
+++ 13/solution.py
@@ -1,5 +1,7 @@
 input = [list(map(int,x.split(": "))) for x in open('data.txt').read().split("\n")]
 
+firewall = []
+
 class Scanner:
 
     movement = True
@@ -14,26 +16,50 @@ class Scanner:
         if self.pos==0: self.movement = True
         if self.pos==self.max-1: self.movement = False
 
-i = 0
-firewall = []
+def ResetFirewall():
+    global firewall
+    i = 0
+    for x in range(input[-1][0]+1):
+        if input[i][0]==x:
+            firewall.append(Scanner(input[i][1]))
+            i += 1
+        else:
+            firewall.append(None)
 
-for x in range(input[-1][0]+1):
-    if input[i][0]==x:
-        firewall.append(Scanner(input[i][1]))
-        i += 1
-    else:
-        firewall.append(None)
-
-# sprawdz, rusz
-
-severity = 0
-
-for packet in range(firewall.__len__()):
-    if firewall[packet] != None:
-        if firewall[packet].pos == 0:
-            severity += packet * firewall[packet].max
+def StepFirewall():
+    global firewall
     for i in firewall:
         if i != None:
             i.Step()
 
-print(severity)
+def ThrowPacket(delay):
+    global firewall
+    severity = 0
+    packet = -delay
+    while packet<firewall.__len__():
+
+        if packet<0:
+            packet += 1
+            StepFirewall()
+            continue
+
+        if firewall[packet] != None:
+            if firewall[packet].pos == 0:
+                severity += packet * firewall[packet].max
+        StepFirewall()
+        
+        packet += 1
+    return severity
+
+
+# First part
+ResetFirewall()
+print("First part: " + str(ThrowPacket(0)))
+
+# Second part
+d = 0
+while ThrowPacket(d)!=0:
+    d += 1
+    ResetFirewall()
+
+print("Second part: " + str(d))
