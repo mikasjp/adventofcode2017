dd6acb1b4fe8a75017598d2807f927edbf997b8b
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-12-15 12:04:41 +0100

    Day 15 - First part done

--- 15/solution.py
+++ 15/solution.py
@@ -1,17 +1,20 @@
 input = [int(x.split(" ")[4]) for x in open("data.txt", "r").read().split("\n")]
 
-A = 16807
-B = 48271
+AF = 16807
+BF = 48271
+
+A = input[0]
+B = input[1]
 
 judge = 0
 
 for i in range(40000000):
-    A *= input[0]
+    A *= AF
     A %= 2147483647
-    B *= input[0]
+    B *= BF
     B %= 2147483647
 
     if A & 0xFFFF == B & 0xFFFF:
         judge +=1
 
-print("First part: " + judge)
+print("First part: " + str(judge))
