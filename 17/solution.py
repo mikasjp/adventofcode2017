20ef5f41d4cb7deee0d415a0f71ca04a7199aa02
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-12-17 16:00:16 +0100

    Day 17 - done

--- 17/solution.py
+++ 17/solution.py
@@ -1,13 +1,31 @@
 input = 359
 
-buffer = [0]
-position = 0
+def Spinlock(jump, iterations):
+    buffer = [0]
+    position = 0
 
-for i in range(2018):
-    position = (position + input) % buffer.__len__()
-    buffer.insert(position+1, i+1)
-    position += 1
+    for i in range(iterations):
+        position = (position + jump) % buffer.__len__()
+        buffer.insert(position+1, i+1)
+        position += 1
+
+    return buffer
 
 # First part
+buffer = Spinlock(input, 2018)
 ans = buffer[(buffer.index(2017) + 1) % buffer.__len__()]
 print("First part: " + str(ans))
+
+# Second part
+bl = 1
+position = 0
+ans = 0
+
+for i in range(1, 50000001):
+    position = (position + input) % bl
+    bl += 1
+    position += 1
+    if (position)==1:
+        ans = i
+
+print("Second part: " + str(ans))
