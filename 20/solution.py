6762e861b5a522ca1d986818b3c3deba43602eee
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-12-20 17:08:36 +0100

    Day 20 - Part 1 done but not as good as I wanted...

--- 20/solution.py
+++ 20/solution.py
@@ -1,22 +1,16 @@
+import math
 import re
 
 input = [[int(s) for s in re.findall("[-\d]+", l)] for l in open("data.txt","r").read().split("\n")]
+acc = [math.sqrt(part[8]**2 + part[7]**2 + part[6]**2) for part in input]
 
 p = [0 for i in range(input.__len__())]
 
-for i, q in enumerate(input):
-    ax = q[6]
-    ay = q[7]
-    az = q[8]
-
-    vx = q[3]
-    vy = q[4]
-    vz = q[5]
-
-    x = q[0] + vx + ax
-    y = q[1] + vy + ay
-    z = q[2] + vz + az
-
-    p[i] += abs(x) + abs(y) + abs(z)
+# First part:
+for w in range(1000):
+    for i, q in enumerate(input):
+        x, y, z, vx, vy, vz, ax, ay, az = q
+        input[i] = [x+vx+ax, y+vy+ay, z+vz+az, vx+ax, vy+ay, vz+az, ax, ay, az]
+        p[i] = abs(input[i][0]) + abs(input[i][1]) + abs(input[i][2])    
 
 print("First part: " + str(p.index(min(p))))
