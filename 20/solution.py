3a2ff3c1b20822f3f83408e765939a07b0c8b4c9
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-12-20 18:09:40 +0100

    Day 20 - Part 1 (more elegant way)

--- 20/solution.py
+++ 20/solution.py
@@ -2,15 +2,18 @@ import math
 import re
 
 input = [[int(s) for s in re.findall("[-\d]+", l)] for l in open("data.txt","r").read().split("\n")]
-acc = [math.sqrt(part[8]**2 + part[7]**2 + part[6]**2) for part in input]
-
-p = [0 for i in range(input.__len__())]
 
 # First part:
-for w in range(1000):
-    for i, q in enumerate(input):
-        x, y, z, vx, vy, vz, ax, ay, az = q
-        input[i] = [x+vx+ax, y+vy+ay, z+vz+az, vx+ax, vy+ay, vz+az, ax, ay, az]
-        p[i] = abs(input[i][0]) + abs(input[i][1]) + abs(input[i][2])    
 
-print("First part: " + str(p.index(min(p))))
+acc = [abs(part[8]) + abs(part[7]) + abs(part[6]) for part in input]
+minacc = [i for i,a in enumerate(acc) if a==min(acc)]
+v = [abs(input[i][5]) + abs(input[i][4]) + abs(input[i][3]) for i in minacc]
+minv = [minacc[i] for i,x in enumerate(v) if x==min(v)]
+distance = [abs(input[i][2]) + abs(input[i][1]) + abs(input[i][0]) for i in minv]
+mindist = [minv[i] for i,x in enumerate(distance) if x==min(distance)]
+
+if mindist.__len__()==1:
+    print("First part: " + str(mindist[0]))
+else:
+    print("Possible solutions: ")
+    print(mindist)
