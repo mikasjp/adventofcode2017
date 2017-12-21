5ea141c412da93c777a3c707a6bd38a982305dbf
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-12-21 20:51:45 +0100

    Day 21 - part 1 (done)

--- 21/solution.py
+++ 21/solution.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 input = [x.split(" => ") for x in open("data.txt","r").read().split("\n")]
 
 def PatternToArray(p):
@@ -28,11 +30,52 @@ def FlipPattern(p1):
     return [p1_xf, p1_yf]
 
 def Compare(p1, p2):
-    p = RotatePattern(p1) + RotatePattern(p1)
-    return p2 in p1
+    flp = FlipPattern(p1)
+    p = RotatePattern(p1) + flp + RotatePattern(flp[0]) + RotatePattern(flp[1])
+    return p2 in p
+
+def FindPattern(item):
+    for x in input:
+        if Compare(PatternToArray(x[0]), item):
+            return PatternToArray(x[1])
+    return False
+            
+
+def ProcessGrid(grid):
+    if grid.__len__()%2 == 0:
+        n = 2
+    else:
+        n = 3
+
+    g = []
+    size = int(grid.__len__() / n)
+    for y in range(size):
+        r = []
+        for x in range(size):
+            combinedpattern = [[grid[n*y + j][n*x + i] for i in range(n)] for j in range(n)]
+            r.append(FindPattern(combinedpattern))
+        r = np.hstack(r).tolist()
+        g.extend(r)
+    return g
+
+
 
 pattern = [[".", "#", "."], [".", ".", "#"], ["#", "#", "#"]]
+#pattern = PatternToArray(".#..#..#./..#..#..#/#########/.#..#..#./..#..#..#/#########/.#..#..#./..#..#..#/#########")
 
-for x in input:
-    if Compare(PatternToArray(x[0]), pattern):
-        print("Mamy to!")
+# Pattern rotation test
+assert RotatePattern([[1, 2], [3, 4]]) == [ [[1, 2], [3, 4]], [[3, 1], [4, 2]], [[4, 3], [2, 1]], [[2, 4], [1, 3]] ]
+# Pattern flipping test
+assert FlipPattern([[1, 2], [3, 4]]) == [ [[3, 4], [1, 2]], [[2, 1], [4, 3]] ]
+
+
+# First part
+
+for i in range(5):
+    pattern = ProcessGrid(pattern)
+
+counter = 0
+for row in pattern:
+    counter += row.count("#")
+
+print("First part: " + str(counter))
