45fdd2d0d1356b102fe12197d8800a31db2eeb96
Mikołaj Kamiński <mkaminski321+github@gmail.com>
2017-12-23 11:29:25 +0100

    Day 22 - part 2 (simplification)

--- 23/input.py
+++ 23/input.py
@@ -1,3 +1,11 @@
+def isPrime(n):
+    ans = True
+    for i in range(2, n-1):
+        if n%i == 0:
+            ans = False
+            break
+    return ans
+
 a, b, c, d, e, f, g, h = 1, 0, 0, 0, 0, 0, 0, 0
 
 b = 93
@@ -10,25 +18,13 @@ c = c + 17000
 
 while True:
     f = 1
-    d = 2
     while True:
-        e = 2
-
-        while True:
-            g = d
-            g = g * e
-            g = g - b
+    
+    for d in range(2, b):
+        for e in range(2,b):
+            g = d * e - b
             if g == 0:
                 f = 0
-            e = e + 1
-            g = e
-            g = g - b
-            if g == 0: break
-
-        d = d + 1
-        g = d
-        g = g - b
-        if g == 0: break
 
     if f == 0:
         h = h + 1
